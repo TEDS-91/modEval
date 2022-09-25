4d96fab13f4ba8ec70695e3bebd9a31e7214e7ec
Tadeu Eder da Silva <tdasilva2@wisc.edu>
2022-09-25 14:21:43 -0500

    solving download button bugs

--- R/data_template_module.R
+++ R/data_template_module.R
@@ -10,12 +10,14 @@ datatemplateUI <- function(id) {
 datatemplateServer <- function(id) {
   moduleServer(id, function(input, output, session) {
 
+    data_template_df <- modEval::data_template
+
     output$downloadBtn <- shiny::downloadHandler(
       filename = function() {
         paste("data_template", ".csv", sep = "")
       },
       content = function(file) {
-        readr::write_csv(data_template, file)
+        readr::write_csv(data_template_df, file)
       }
     )
 
