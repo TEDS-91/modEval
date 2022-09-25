4e4bae8d3c9ca3c5c392ad72c239ca3a337d92b9
Tadeu Eder da Silva <tdasilva2@wisc.edu>
2022-09-25 10:07:12 -0500

    creating function for plotting

--- R/pred_vs_obs_module.R
+++ R/pred_vs_obs_module.R
@@ -11,41 +11,43 @@ predobsServer <- function(id, dataset) {
 
     output$pred_vs_obs_viz <- shiny::renderPlot({
 
-      df <- dataset() %>%
-        tibble::as_tibble()
+      plot_pred_obs(dataset())
 
-      df %>%
-        tidyr::pivot_longer(
-          cols = -1,
-          names_to = "models",
-          values_to = "predicted"
-        ) %>%
-        dplyr::mutate(
-          residuals = observed - predicted
-        ) %>%
-        dplyr::arrange(models) %>%
-        dplyr::relocate(models, .before = observed) %>%
-        ggplot2::ggplot() +
-        ggplot2::theme_bw() +
-        ggplot2::geom_point(ggplot2::aes(x = predicted, y = observed, color = "Observed")) +
-        ggplot2::geom_abline(slope = 1, intercept = 0, color = "blue") +
-        ggplot2::geom_point(ggplot2::aes(x = predicted, y = residuals, color = "Residuals")) +
-        ggplot2::geom_abline(slope = 0, intercept = 0, color = "black") +
-        ggplot2::facet_wrap(~ models) +
-        ggplot2::xlab("Predicted Values") +
-        ggplot2::ylab("Observed Values") +
-        ggplot2::scale_color_manual(name   = " ",
-                                    breaks = c("Observed",
-                                               "Regression Line (a + bx)",
-                                               "Residuals",
-                                               "Regression Line for Residuals"),
-                                    values = c("Observed"        = "black",
-                                               "Regression Line (a + bx)" = "blue",
-                                               "Residuals"       = "red",
-                                               "Regression Line for Residuals" = "black")) +
-        ggplot2::theme(legend.position = "bottom",
-                       text            = ggplot2::element_text(size = 14),
-                       legend.text     = ggplot2::element_text(size = 14))
+      # df <- dataset() %>%
+      #   tibble::as_tibble()
+      #
+      # df %>%
+      #   tidyr::pivot_longer(
+      #     cols = -1,
+      #     names_to = "models",
+      #     values_to = "predicted"
+      #   ) %>%
+      #   dplyr::mutate(
+      #     residuals = observed - predicted
+      #   ) %>%
+      #   dplyr::arrange(models) %>%
+      #   dplyr::relocate(models, .before = observed) %>%
+      #   ggplot2::ggplot() +
+      #   ggplot2::theme_bw() +
+      #   ggplot2::geom_point(ggplot2::aes(x = predicted, y = observed, color = "Observed")) +
+      #   ggplot2::geom_abline(slope = 1, intercept = 0, color = "blue") +
+      #   ggplot2::geom_point(ggplot2::aes(x = predicted, y = residuals, color = "Residuals")) +
+      #   ggplot2::geom_abline(slope = 0, intercept = 0, color = "black") +
+      #   ggplot2::facet_wrap(~ models) +
+      #   ggplot2::xlab("Predicted Values") +
+      #   ggplot2::ylab("Observed Values") +
+      #   ggplot2::scale_color_manual(name   = " ",
+      #                               breaks = c("Observed",
+      #                                          "Regression Line (a + bx)",
+      #                                          "Residuals",
+      #                                          "Regression Line for Residuals"),
+      #                               values = c("Observed"        = "black",
+      #                                          "Regression Line (a + bx)" = "blue",
+      #                                          "Residuals"       = "red",
+      #                                          "Regression Line for Residuals" = "black")) +
+      #   ggplot2::theme(legend.position = "bottom",
+      #                  text            = ggplot2::element_text(size = 14),
+      #                  legend.text     = ggplot2::element_text(size = 14))
 
 
     }, height = 700, width = 900)
