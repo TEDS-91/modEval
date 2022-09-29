26648efd72315aa38a94102357b9d576ce7efda5
Tadeu Eder da Silva <tdasilva2@wisc.edu>
2022-09-28 22:22:07 -0500

    adjusting metrics and explanation

--- R/model_metrics_explanation_module.R
+++ R/model_metrics_explanation_module.R
@@ -2,16 +2,16 @@
 model_explanationUI <- function(id) {
   shiny::tagList(
 
-    shiny::tags$p(shiny::tags$b("Intercept and Slope:"), "Regressions coefficients."),
-    shiny::tags$p(shiny::tags$b("r:"), " Pearson correlation."),
-    shiny::tags$p(shiny::tags$b("r2:"), "Coefficient of determination."),
-    shiny::tags$p(shiny::tags$b("MB:"), "Mean bias."),
-    shiny::tags$p(shiny::tags$b("MAE:"), "Mean absolute error."),
-    shiny::tags$p(shiny::tags$b("MSE:"), "Mean square error."),
-    shiny::tags$p(shiny::tags$b("RMSE:"), "Root mean square."),
-    shiny::tags$p(shiny::tags$b("CCC:"), "Concordance correlation coefficient."),
-    shiny::tags$p(shiny::tags$b("CD:"), "Coefficient of model determination."),
-    shiny::tags$p(shiny::tags$b("ME:"), "Modeling efficiency."),
+    shiny::tags$p(shiny::tags$b("Intercept and Slope:"), "Regressions coefficients.",
+    shiny::tags$b("r:"), " Pearson correlation.",
+    shiny::tags$b("r2:"), "Coefficient of determination.",
+    shiny::tags$b("MB:"), "Mean bias.",
+    shiny::tags$b("MAE:"), "Mean absolute error.",
+    shiny::tags$b("MSE:"), "Mean square error.",
+    shiny::tags$b("RMSE:"), "Root mean square.",
+    shiny::tags$b("CCC:"), "Concordance correlation coefficient.",
+    shiny::tags$b("CD:"), "Coefficient of model determination.",
+    shiny::tags$b("ME:"), "Modeling efficiency."),
 
     shiny::br(),
 
